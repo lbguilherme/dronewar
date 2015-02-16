9f50b10889f8e5617498b355e89cd64130718a80
Physicist <physicist093@yahoo.com.br>
2015-02-16 10:46:16 -0300

    Fix #4. Math namespace in cte.hpp

--- modules/math/include/cte.hpp
+++ modules/math/include/cte.hpp
@@ -2,7 +2,9 @@
 
 #include <math/Real>
 
+namespace math {
 namespace cte {
 	constexpr math::Real pi = 3.14159265358979323;	// Pi
 	constexpr math::Real e = 2.718281828459045235;	// Euler number
-};
+};  // cte namespace
+};	// math namespace
