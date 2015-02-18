503c4e4d5aab48a7a2ea4b59356f59eb52468721
GuilhermeBernal <dev@lbguilherme.com>
2015-02-18 10:09:21 -0300

    Added make task to check all headers before compilation

--- mk/module.cpp.mk
+++ mk/module.cpp.mk
@@ -10,10 +10,13 @@ LIBRARY := $(BUILD)/lib$(MODULE).a
 CXX = clang++ -g -std=c++14 -Wall -Wextra -I$(BUILD)/include -fdiagnostics-color=auto
 AR = ar rcs
 
-build: $(LIBRARY)
+build: prepare check $(LIBRARY)
 	@:
 
 prepare: $(patsubst include/%.hpp,$(BUILD)/include/$(MODULE)/%,$(INCLUDES))
+	@:
+	
+check: prepare $(patsubst include/%.hpp,$(BUILD)/include/$(MODULE)/%.hpp_checked,$(INCLUDES))
 	@:
 
 $(BUILD)/include/$(MODULE)/%: include/%.hpp $(BUILD)/include/$(MODULE)/%.hpp
@@ -23,6 +26,13 @@ $(BUILD)/include/$(MODULE)/%: include/%.hpp $(BUILD)/include/$(MODULE)/%.hpp
 $(BUILD)/include/$(MODULE)/%.hpp: include/%.hpp
 	@mkdir -p $(dir $@)
 	@cp $< $(dir $@)
+
+$(BUILD)/include/$(MODULE)/%.hpp_checked: include/%.hpp
+	@echo "Checking  "$<" ..."
+	@echo "#include <$(MODULE)/"$<">" > $@.cpp
+	@cd .. && $(CXX) -Wno-pragmas -fsyntax-only -I$(MODULE)/$(BUILD)/include -I. $(MODULE)/$@.cpp
+	@rm $@.cpp
+	@touch $@
 
 $(BUILD)/deps/$(MODULE)/%.d: src/%.cpp prepare
 	@mkdir -p $(dir $@)
