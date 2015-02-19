c7bcf3bb06adebbd677bd98a6f37524753396f9c
GuilhermeBernal <dev@lbguilherme.com>
2015-02-19 13:46:59 -0300

    Add initial test for Vector

---
+++ modules/math/test/Vector.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <math/Vector>
+
+using math::Vector;
+
+TEST(Vector, length) {
+	Vector v = {3, 4, 0};
+	EXPECT_DOUBLE_EQ(5, v.length());
+	EXPECT_DOUBLE_EQ(5, (-v).length());
+}
+
