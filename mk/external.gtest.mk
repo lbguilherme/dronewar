8c34dbb81e9f56760b71ff8c0d9fed2e9e6fb274
GuilhermeBernal <dev@lbguilherme.com>
2015-02-19 14:31:46 -0300

    Build gtest using clang

--- mk/external.gtest.mk
+++ mk/external.gtest.mk
@@ -7,7 +7,7 @@ gtest: $(LIBGTEST)
 $(LIBGTEST):
 	@echo "Building   gtest ..."
 	@mkdir -p $(BUILD)/external/gtest
-	@cd $(BUILD)/external/gtest && cmake ../../../external/gtest > /dev/null
+	@cd $(BUILD)/external/gtest && cmake ../../../external/gtest -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang > /dev/null
 	@cd $(BUILD)/external/gtest && $(MAKE) > /dev/null
 	@mkdir -p $(BUILD)/external/include
 	@cp -r $(BUILD)/../external/gtest/include/gtest $(BUILD)/external/include
