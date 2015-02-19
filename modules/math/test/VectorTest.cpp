076c96ae92234432f387e3ecc22a968bfc0e4ca9
GuilhermeBernal <dev@lbguilherme.com>
2015-02-19 15:58:50 -0300

    Refactor Vector test

--- modules/math/test/VectorTest.cpp
+++ modules/math/test/VectorTest.cpp
@@ -1,9 +1,10 @@
 #include <gtest/gtest.h>
+
 #include <math/Vector>
 
 using math::Vector;
 
-TEST(Vector, length) {
+TEST(Vector, LengthComputation) {
 	Vector v = {3, 4, 0};
 	EXPECT_DOUBLE_EQ(5, v.length());
 	EXPECT_DOUBLE_EQ(5, (-v).length());
