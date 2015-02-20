c9908220c34ad63b2fb2d718c515ac2f434a1742
Physicist <physicist093@yahoo.com.br>
2015-02-20 18:37:23 -0300

    Fixing the Matrix test

--- modules/math/test/MatrixTest.cpp
+++ modules/math/test/MatrixTest.cpp
@@ -9,59 +9,59 @@ struct MatrixAlgebra : public ::testing::Test {
 };
 
 TEST_F(MatrixAlgebra, Zeros) {
+	EXPECT_DOUBLE_EQ(0, null(0, 0));
+	EXPECT_DOUBLE_EQ(0, null(0, 1));
+	EXPECT_DOUBLE_EQ(0, null(0, 2));
+	
+	EXPECT_DOUBLE_EQ(0, null(1, 0));
 	EXPECT_DOUBLE_EQ(0, null(1, 1));
 	EXPECT_DOUBLE_EQ(0, null(1, 2));
-	EXPECT_DOUBLE_EQ(0, null(1, 3));
 	
+	EXPECT_DOUBLE_EQ(0, null(2, 0));
 	EXPECT_DOUBLE_EQ(0, null(2, 1));
 	EXPECT_DOUBLE_EQ(0, null(2, 2));
-	EXPECT_DOUBLE_EQ(0, null(2, 3));
-	
-	EXPECT_DOUBLE_EQ(0, null(3, 1));
-	EXPECT_DOUBLE_EQ(0, null(3, 2));
-	EXPECT_DOUBLE_EQ(0, null(3, 3));
 }
 
 TEST_F(MatrixAlgebra, Ones) {
+	EXPECT_DOUBLE_EQ(1, one(0, 0));
+	EXPECT_DOUBLE_EQ(1, one(0, 1));
+	EXPECT_DOUBLE_EQ(1, one(0, 2));
+	
+	EXPECT_DOUBLE_EQ(1, one(1, 0));
 	EXPECT_DOUBLE_EQ(1, one(1, 1));
 	EXPECT_DOUBLE_EQ(1, one(1, 2));
-	EXPECT_DOUBLE_EQ(1, one(1, 3));
 	
+	EXPECT_DOUBLE_EQ(1, one(2, 0));
 	EXPECT_DOUBLE_EQ(1, one(2, 1));
 	EXPECT_DOUBLE_EQ(1, one(2, 2));
-	EXPECT_DOUBLE_EQ(1, one(2, 3));
-	
-	EXPECT_DOUBLE_EQ(1, one(3, 1));
-	EXPECT_DOUBLE_EQ(1, one(3, 2));
-	EXPECT_DOUBLE_EQ(1, one(3, 3));
 }
 
 TEST_F(MatrixAlgebra, Identity) {
+	EXPECT_DOUBLE_EQ(1, identity(0, 0));
+	EXPECT_DOUBLE_EQ(0, identity(0, 1));
+	EXPECT_DOUBLE_EQ(0, identity(0, 2));
+	
+	EXPECT_DOUBLE_EQ(0, identity(1, 0));
 	EXPECT_DOUBLE_EQ(1, identity(1, 1));
 	EXPECT_DOUBLE_EQ(0, identity(1, 2));
-	EXPECT_DOUBLE_EQ(0, identity(1, 3));
 	
+	EXPECT_DOUBLE_EQ(0, identity(2, 0));
 	EXPECT_DOUBLE_EQ(0, identity(2, 1));
 	EXPECT_DOUBLE_EQ(1, identity(2, 2));
-	EXPECT_DOUBLE_EQ(0, identity(2, 3));
-	
-	EXPECT_DOUBLE_EQ(0, identity(3, 1));
-	EXPECT_DOUBLE_EQ(0, identity(3, 2));
-	EXPECT_DOUBLE_EQ(1, identity(3, 3));
 }
 
 TEST_F(MatrixAlgebra, Equality) {
 	Matrix mat = identity + one;
 
+	EXPECT_DOUBLE_EQ(2, mat(0, 0));
+	EXPECT_DOUBLE_EQ(1, mat(0, 1));
+	EXPECT_DOUBLE_EQ(1, mat(0, 2));
+	
+	EXPECT_DOUBLE_EQ(1, mat(1, 0));
 	EXPECT_DOUBLE_EQ(2, mat(1, 1));
 	EXPECT_DOUBLE_EQ(1, mat(1, 2));
-	EXPECT_DOUBLE_EQ(1, mat(1, 3));
 	
+	EXPECT_DOUBLE_EQ(1, mat(2, 0));
 	EXPECT_DOUBLE_EQ(1, mat(2, 1));
 	EXPECT_DOUBLE_EQ(2, mat(2, 2));
-	EXPECT_DOUBLE_EQ(1, mat(2, 3));
-	
-	EXPECT_DOUBLE_EQ(1, mat(3, 1));
-	EXPECT_DOUBLE_EQ(1, mat(3, 2));
-	EXPECT_DOUBLE_EQ(2, mat(3, 3));
 }
