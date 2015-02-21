2a1d5e19e321be358a6bf6893789dfc532487378
GuilhermeBernal <dev@lbguilherme.com>
2015-02-21 14:04:41 -0300

    Makefile.latex: Provide better output and remove useless files

--- mk/calcs.latex.mk
+++ mk/calcs.latex.mk
@@ -2,9 +2,11 @@
 PDFLATEX := pdflatex -interaction nonstopmode -halt-on-error -file-line-error
 
 build/calcs.pdf: calcs/calcs.tex $(wildcard calcs/**/*.tex) mk/calcs.latex.mk
+	@echo "Producing  $@ ..."
 	@mkdir -p $(dir $@)
 	-@cd calcs && ( \
 		$(PDFLATEX) -output-directory=../$(dir $@) calcs.tex && \
 		$(PDFLATEX) -output-directory=../$(dir $@) calcs.tex && \
 		$(PDFLATEX) -output-directory=../$(dir $@) calcs.tex \
 	) | grep ^! | cat
+	@rm -f `ls build/calcs.* | grep -v pdf`
