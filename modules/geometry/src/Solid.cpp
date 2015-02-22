473bf8ba0d6330ccbc6e1c189a5ee54340da9668
Physicist <physicist093@yahoo.com.br>
2015-02-21 21:00:50 -0300

    Conclusion of orientation function on Solid class

--- modules/geometry/src/Solid.cpp
+++ modules/geometry/src/Solid.cpp
@@ -1,11 +1,31 @@
 #include <geometry/Solid>
+#include <math/Matrix>
 
 using namespace math;
 using namespace geometry;
 
 
 void Solid::orient() {
-	throw "not implemented";
+	for (Triangle face : triangles()) {
+	
+		unsigned counter = 0;
+		for (Triangle triangle : triangles()) {
+			Vector p = face.vector() - triangle.vertices()[0].vector();
+			Vector e1 = triangle.vertices()[1].vector() - triangle.vertices()[0].vector();
+			Vector e2 = triangle.vertices()[2].vector() - triangle.vertices()[0].vector();
+			
+			Matrix mat(-triangle.normal(), e1, e2);
+			Vector solution = mat.inverse() * p;
+			
+			if (solution.y() < 0) continue;
+			if (solution.z() < 0) continue;
+			if (solution.y() > 1) continue;
+			if (solution.z() > 1) continue;
+			++counter;
+		}
+		
+		if (counter % 2) face.changeOrientation();
+	}
 }
 
 Real Solid::volume() const {
