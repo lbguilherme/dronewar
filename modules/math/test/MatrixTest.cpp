5d06d3fd8f6b9b4d90767014940095f3290d7222
Physicist <physicist093@yahoo.com.br>
2015-02-21 21:37:06 -0300

    Add some matrix-vector tests

--- modules/math/test/MatrixTest.cpp
+++ modules/math/test/MatrixTest.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <math/Matrix>
+#include <math/Vector>
 using math::Matrix;
+using math::Vector;
 
 struct MatrixAlgebra : public ::testing::Test {
 	Matrix identity = Matrix::eye();
@@ -129,4 +131,54 @@ TEST_F(MatrixAlgebra, Inverse) {
 	EXPECT_DOUBLE_EQ(0, idiv(2, 0));
 	EXPECT_DOUBLE_EQ(0, idiv(2, 1));
 	EXPECT_DOUBLE_EQ(1, idiv(2, 2));
+}
+
+
+struct MatrixVector : public ::testing::Test {
+	Matrix identity = Matrix::eye();
+	Matrix one = Matrix::ones();
+	
+	Vector i = {1, 0, 0};
+	Vector j = {0, 1, 0};
+	Vector k = {0, 0, 1};
+};
+
+TEST_F(MatrixVector, Construction) {
+	Matrix tobuild(i, j, k);
+	
+	EXPECT_DOUBLE_EQ(1, tobuild(0, 0));
+	EXPECT_DOUBLE_EQ(0, tobuild(0, 1));
+	EXPECT_DOUBLE_EQ(0, tobuild(0, 2));
+	
+	EXPECT_DOUBLE_EQ(0, tobuild(1, 0));
+	EXPECT_DOUBLE_EQ(1, tobuild(1, 1));
+	EXPECT_DOUBLE_EQ(0, tobuild(1, 2));
+	
+	EXPECT_DOUBLE_EQ(0, tobuild(2, 0));
+	EXPECT_DOUBLE_EQ(0, tobuild(2, 1));
+	EXPECT_DOUBLE_EQ(1, tobuild(2, 2));
+}
+
+TEST_F(MatrixVector, Multiplication) {
+	Vector test;
+	
+	test = 2 * one * i;
+	EXPECT_DOUBLE_EQ(2, test(0));
+	EXPECT_DOUBLE_EQ(2, test(1));
+	EXPECT_DOUBLE_EQ(2, test(2));
+	
+	test = 3 * identity * i;
+	EXPECT_DOUBLE_EQ(3, test(0));
+	EXPECT_DOUBLE_EQ(0, test(1));
+	EXPECT_DOUBLE_EQ(0, test(2));
+	
+	test = identity * j;
+	EXPECT_DOUBLE_EQ(0, test(0));
+	EXPECT_DOUBLE_EQ(1, test(1));
+	EXPECT_DOUBLE_EQ(0, test(2));
+	
+	test = identity * k;
+	EXPECT_DOUBLE_EQ(0, test(0));
+	EXPECT_DOUBLE_EQ(0, test(1));
+	EXPECT_DOUBLE_EQ(1, test(2));
 }
