751576248730083bdd4cf70905d978a97c6e9ed9
GuilhermeBernal <dev@lbguilherme.com>
2015-02-25 13:22:51 -0300

    Simplify triangle test

--- modules/geometry/test/TriangleTest.cpp
+++ modules/geometry/test/TriangleTest.cpp
@@ -1,20 +1,12 @@
 #include <gtest/gtest.h>
 
-#include <math/Real>
-#include <math/Vector>
-#include <geometry/Solid>
+#include <geometry/Mesh>
 #include <geometry/Vertex>
-#include <geometry/Edge>
 #include <geometry/Triangle>
 
-using math::Real;
-using math::Vector;
 using geometry::Mesh;
-using geometry::Solid;
 using geometry::Vertex;
-using geometry::Edge;
 using geometry::Triangle;
-
 
 TEST(Triangle, Area) {
 	Mesh mesh;
@@ -23,11 +15,7 @@ TEST(Triangle, Area) {
 	Vertex v2 = mesh.addVertex({1, 1, 0});
 	Vertex v3 = mesh.addVertex({2, 0, 0});
 	
-	Edge e12 = mesh.addEdge(v1, v2);
-	Edge e13 = mesh.addEdge(v1, v3);
-	Edge e23 = mesh.addEdge(v2, v3);
-	
-	Triangle triangle = mesh.addTriangle(e12, e13, e23);
+	Triangle triangle = mesh.addTriangle(v1, v2, v3);
 	
 	EXPECT_DOUBLE_EQ(1, triangle.area());
 }
