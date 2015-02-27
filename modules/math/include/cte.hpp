82289cb7bbeff2b39f2fed7809ce38880dce69fb
Physicist <physicist093@yahoo.com.br>
2015-02-27 14:57:59 -0300

    Add tau as mathematical constant

--- modules/math/include/cte.hpp
+++ modules/math/include/cte.hpp
@@ -5,8 +5,9 @@
 namespace math {
 namespace cte {
 
-constexpr math::Real pi = 3.14159265358979323;	// Pi
-constexpr math::Real e = 2.718281828459045235;	// Euler number
+constexpr math::Real tau = 6.28318530717958647692;	// Tau
+constexpr math::Real pi = 3.14159265358979323;		// Pi
+constexpr math::Real e = 2.718281828459045235;		// Euler number
 
 };  // cte namespace
 };	// math namespace
